42019cc9dfe0369f61f38bcaba0bd1ea4f18d6a4
Danylo <Danylo.Brudnyi.RI.2023@lpnu.ua>
2023-10-08 13:30:43 +0300

    my first commit

---
+++ PR-04.2.cpp
@@ -0,0 +1,55 @@
+// PR-04.2.cpp
+// < Брудний, Данило >
+// Лабораторна робота № 4.2
+// Розгалуження, задане плоскою фігурою.
+// Варіант 3
+
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    double x, xp, xk, dx, A, B, y;
+
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+
+    cout << fixed;
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(5) << "x" << " |"
+        << setw(7) << "y" << " |" << endl;
+    cout << "---------------------------" << endl;
+
+    x = xp;
+
+    while (x <= xk)
+    {
+        A = 2 / x + abs(x);
+
+        if (x < 0)
+            B = 1 + 4 * x * x;
+        else
+        {
+            if (x >= 0 && x <= 2)
+                B = pow((exp(x) + abs(x)), 2);
+            else
+                B = 5 * sin(x * x + 1);
+        }
+
+        y = A + B;
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << y
+            << " |" << endl;
+
+        x += dx;
+    }
+    cout << "---------------------------" << endl;
+
+
+    return 0;
+}
